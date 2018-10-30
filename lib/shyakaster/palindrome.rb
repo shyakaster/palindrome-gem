6a54307884b13fe9a604ba9df08aab932ed8e8f2
alexshyaka <alex.nkusi@andela.com>
2018-10-30 17:39:19 +0300

    Initialize palindrome gem repo

---
+++ lib/shyakaster/palindrome.rb
@@ -0,0 +1,7 @@
+require "shyakaster/palindrome/version"
+
+module Shyakaster
+  module Palindrome
+    # Your code goes here...
+  end
+end
---
+++ lib/shyakaster/palindrome/version.rb
@@ -0,0 +1,5 @@
+module Shyakaster
+  module Palindrome
+    VERSION = "0.1.0"
+  end
+end
